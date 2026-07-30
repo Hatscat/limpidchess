07c079c0a18282e77156ec8de9c3c20248374047
Lucien Boudy <luboudy@gmail.com>
2026-07-30 21:05:55 +0200

    feat: add input listener removal tracking for canvas interactions

--- docs/play/index.service.worker.js
+++ docs/play/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785436874|4091244';
+const CACHE_VERSION = '1785438324|3842498';
 /** @type {string} */
 const CACHE_PREFIX = 'Limpid Chess-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
