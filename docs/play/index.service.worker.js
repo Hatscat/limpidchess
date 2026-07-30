2be299a7ddefae5351afb460e6c2f25b3a3339e1
Lucien Boudy <luboudy@gmail.com>
2026-07-30 17:35:12 +0200

    feat: add boot diagnostics and self-heal for web build; update service worker cache version

--- docs/play/index.service.worker.js
+++ docs/play/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1784367739|3661519';
+const CACHE_VERSION = '1785425577|4910678';
 /** @type {string} */
 const CACHE_PREFIX = 'Limpid Chess-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
