31cb80d584330cfccdf1148f06f1ac23d44f04c8
Lucien Boudy <luboudy@gmail.com>
2026-07-31 14:38:15 +0200

    feat: update CACHE_VERSION for service worker to refresh cached resources

--- docs/play/index.service.worker.js
+++ docs/play/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785500059|4168825';
+const CACHE_VERSION = '1785501467|3956820';
 /** @type {string} */
 const CACHE_PREFIX = 'Limpid Chess-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
