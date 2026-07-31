d7740e43d9e279ae53443fee891e6442543a35f8
Lucien Boudy <luboudy@gmail.com>
2026-07-31 12:43:00 +0200

    feat: implement compositing fix for iOS portrait freeze with transform adjustments

--- docs/play/index.service.worker.js
+++ docs/play/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785493193|3984423';
+const CACHE_VERSION = '1785494552|4001762';
 /** @type {string} */
 const CACHE_PREFIX = 'Limpid Chess-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
