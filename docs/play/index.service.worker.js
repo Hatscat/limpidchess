f30bb09c7e384e81fd6d50f158f1f7db790af9e0
Lucien Boudy <luboudy@gmail.com>
2026-08-02 15:07:02 +0200

    feat: enhance WebGL error diagnostics and implement logging for canvas state

--- docs/play/index.service.worker.js
+++ docs/play/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785667352|3781104';
+const CACHE_VERSION = '1785675982|4713841';
 /** @type {string} */
 const CACHE_PREFIX = 'Limpid Chess-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
