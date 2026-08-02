5c13308d2494a82dd7edba422664eb1f2ecda305
Lucien Boudy <luboudy@gmail.com>
2026-08-02 20:09:04 +0200

    feat: add WebGL draw call instrumentation for diagnosing iOS portrait freeze issues

--- docs/play/index.service.worker.js
+++ docs/play/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785675982|4713841';
+const CACHE_VERSION = '1785691313|3897729';
 /** @type {string} */
 const CACHE_PREFIX = 'Limpid Chess-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
