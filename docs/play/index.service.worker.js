b4533da725b17b0fbb646c63aa3f6589a3351e10
Lucien Boudy <luboudy@gmail.com>
2026-08-02 12:43:04 +0200

    feat: implement lowdpi handling for canvas rendering and update CACHE_VERSION for service worker

--- docs/play/index.service.worker.js
+++ docs/play/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785504842|4072412';
+const CACHE_VERSION = '1785667352|3781104';
 /** @type {string} */
 const CACHE_PREFIX = 'Limpid Chess-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
