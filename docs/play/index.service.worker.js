a44d0048733d112c4dbd5576a58684352dd04c19
Lucien Boudy <luboudy@gmail.com>
2026-08-03 10:47:37 +0200

    feat: add iOS portrait notice to mitigate WebGL canvas freeze issue; update CACHE_VERSION for service worker

--- docs/play/index.service.worker.js
+++ docs/play/index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1785743072|3703051';
+const CACHE_VERSION = '1785745348|5067900';
 /** @type {string} */
 const CACHE_PREFIX = 'Limpid Chess-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
